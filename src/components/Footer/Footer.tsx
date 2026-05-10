64952ba9464fa78c0edfd3ce5c7c9e774aac893f
Leonardo Rimes <rimesleo@gmail.com>
2026-05-09 23:19:03 -0300

    add links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import styles from "./Footer.module.css";
+import Link from "next/link";
 
 export default function Footer() {
   const currentYear = new Date().getFullYear();
@@ -16,9 +17,16 @@ export default function Footer() {
 
         {/* Right Copyright */}
         <div className={styles.side}>
-          <p className={styles.copyright}>
-            © {currentYear} REINN. All rights reserved.
-          </p>
+          <div className={styles.copyrightContainer}>
+            <p className={styles.copyright}>
+              © {currentYear} REINN. All rights reserved.
+            </p>
+            <div className={styles.legalLinks}>
+              <Link href="/privacy-policy" className={styles.legalLink}>Privacy Policy</Link>
+              <span className={styles.separator}>|</span>
+              <Link href="/terms-and-conditions" className={styles.legalLink}>Terms and conditions</Link>
+            </div>
+          </div>
         </div>
       </div>
     </footer>
